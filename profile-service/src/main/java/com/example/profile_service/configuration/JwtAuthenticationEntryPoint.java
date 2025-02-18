cd7574912cfe905f9d6d7885146efe2427542bf9
Tien <73010955+tientran505@users.noreply.github.com>
2025-02-18 23:38:15 +0700

    feat: add author

---
+++ profile-service/src/main/java/com/example/profile_service/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.example.profile_service.configuration;
+
+import com.example.profile_service.dto.response.ApiResponse;
+import com.example.profile_service.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException {
+        ErrorCode errorCode = ErrorCode.USER_UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getStatusCode().value());
+        response.setContentType("application/json");
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
---
+++ profile-service/src/main/java/com/example/profile_service/dto/response/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.example.profile_service.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    int code = 200;
+    String message;
+    T result;
+}
